22d0fa99832ff837d252f336658b73c0708a95c8
Mici <villanuevapaezchristian77@gmail.com>
2021-10-08 23:54:18 -0500

    añadido pagina proximos estrenos, funcionando con vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+    <ul>
+      <router-link to="/"
+        >Home
+        <img src="assets\img\logo2.png">
+      </router-link>
+      <li>
+        <a href="página principal.html">INICIO</a>
+      </li>
+      <li>
+        <router-link to="/cartelera">CARTELERA</router-link>
+      </li>
+      <li>
+        <router-link to="/proximos_estrenos">PROXIMOS ESTRENOS</router-link>
+      </li>
+      <li>
+        <a href="comidas.html">COMIDA</a>
+      </li>
+      <li>
+        <a href="Tarifas.html">TARIFAS</a>
+      </li>
+      <li>
+        <a href="Inicio.html">INICIAR SESION</a>
+      </li>
+    </ul>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -17,8 +40,10 @@
 
 #nav {
   padding: 30px;
+  grid-column: 1;
+  text-align: center;
 }
-
+/*
 #nav a {
   font-weight: bold;
   color: #2c3e50;
@@ -26,5 +51,25 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+} */
+
+div ul {
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  align-items: center;
+  text-decoration: none;
+}
+
+div li {
+  display: inline-block;
+  border: 1px solid blue;
+  border-radius: 16px;
+  margin: 2px;
+  padding: 2px;
+  background-color: #57cc99;
+
+  text-decoration: none;
 }
 </style>
---
+++ src/views/proximos_estrenos.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="home">
+    <body>
+      <main>
+        <header>
+          <h1>Proximos Estrenos</h1>
+        </header>
+        <ul>
+          <li>
+            <h1>Sin Tiempo para morir</h1>
+            <img
+              class="poster"
+              src="..\assets\img\proximos_estrenos\sin_tiempo_para_morir_poster_480x670.jpg"
+            />
+          </li>
+          <li>
+            <h1>Dog Rock: Renace Una Estrella</h1>
+            <img
+              class="poster"
+              src="..\assets\img\proximos_estrenos\rock_dog_poster_480x670.png"
+            />
+          </li>
+          <li>
+            <h1>Ron da Error</h1>
+            <img
+              class="poster"
+              src="..\assets\img\proximos_estrenos\ron_da_error_poster_480x670.png"
+            />
+          </li>
+          <li>
+            <h1>Miss Revolución</h1>
+            <img
+              class="poster"
+              src="..\assets\img\proximos_estrenos\miss_revolucion_poster_480x670.jpg"
+            />
+          </li>
+          <li>
+            <h1>Chernóbil: La Pelicula</h1>
+            <img
+              class="poster"
+              src="..\assets\img\proximos_estrenos\chernobil_poster_480x670.png"
+            />
+          </li>
+          <li>
+            <h1>La Leyenda de la Viuda</h1>
+            <img
+              class="poster"
+              src="..\assets\img\proximos_estrenos\la_leyenda_de_la_viuda_poster_480x670.png"
+            />
+          </li>
+        </ul>
+      </main>
+    </body>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+</script>
+
+<style scoped>
+body {
+  background-color: #80ed99;
+}
+
+a {
+  text-decoration: none;
+}
+
+nav {
+  grid-column: 1;
+  text-align: center;
+}
+
+#logo {
+  border: 1px solid grey;
+  border-radius: 4px;
+}
+
+nav ul {
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  align-items: center;
+  text-decoration: none;
+}
+
+nav li {
+  display: inline-block;
+  border: 1px solid blue;
+  border-radius: 16px;
+  margin: 2px;
+  padding: 2px;
+  background-color: #57cc99;
+
+  text-decoration: none;
+}
+
+header {
+  text-align: center;
+}
+
+main li {
+  border: 2px solid blue;
+  border-radius: 4px;
+  margin: 2px;
+  padding: 10px;
+  background-color: #57cc99;
+  box-shadow: 4;
+  color: #fff8e5;
+}
+
+.poster {
+  border-radius: 8px;
+}
+
+@media screen and (min-width: 1024px) {
+  main ul {
+    display: grid;
+    grid-template-rows: 50% 50%;
+    grid-template-columns: 33% 33% 33%;
+    align-items: center;
+    text-align: center;
+    list-style-type: none;
+  }
+
+  main li {
+    display: inline-table;
+  }
+}
+
+@media screen and (min-width: 360px) and (max-width: 1024px) {
+  main li {
+    font-size: 12px;
+  }
+
+  main ul {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr;
+    align-items: center;
+    text-align: center;
+    list-style-type: none;
+  }
+}
+</style>
