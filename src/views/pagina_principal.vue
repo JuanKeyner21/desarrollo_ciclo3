1a155efbeaa7f6c8b8b5ee3bf5d5595845632867
Juan Pablo <juan.diaz3@o365.unab.edu.co>
2021-10-13 20:19:25 -0500

    Se agragan las imágenes de la página principal en VUE

--- src/views/pagina_principal.vue
+++ src/views/pagina_principal.vue
@@ -7,15 +7,15 @@
   <ul>
     <li>
       <h1>La Última Estafa</h1>
-      <img class="poster" src="img\cartelera\poster la ultima estafa.png" />
+      <img class="poster" height="450" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cinecolombia.com%2Fpeliculas%2Fla-ultima-estafa&psig=AOvVaw2zpN0EdPwd9j7Lj9efbSGa&ust=1634260418845000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCKin7NLcyPMCFQAAAAAdAAAAABAD" />
     </li>
     <li>
       <h1>Shang-Chi</h1>
-      <img class="poster" src="img\cartelera\poster shang-chi.jpg" />
+      <img class="poster" height="450" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fnoescinetodoloquereluce.com%2F2021%2F08%2Fnuevo-poster-de-shang-chi-y-la-leyenda-de-los-diez-anillos-la-cinta-de-artes-marciales-de-marvel.html&psig=AOvVaw3Oo3qMpSAaZr3ncs9NABTt&ust=1634260523721000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCIjFmIPdyPMCFQAAAAAdAAAAABAD" />
     </li>
     <li>
       <h1>Maligno</h1>
-      <img class="poster" src="img\cartelera\poster maligno.jpg" />
+      <img class="poster" height="450" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dodmagazine.es%2Fmaligno-poster-oficialpelicula-terror-james-wan%2F&psig=AOvVaw1psJYyg0kQTm96epz--a9X&ust=1634260549310000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCNCgn5DdyPMCFQAAAAAdAAAAABAD" />
     </li>
   </ul>
 
@@ -24,22 +24,22 @@
     <li>
       <h1>Sin Tiempo para morir</h1>
       <img
-        class="poster"
-        src="img\proximos_estrenos\sin_tiempo_para_morir_poster_480x670.jpg"
+        class="poster" height="450"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sensacine.com%2Fpeliculas%2Fpelicula-212358%2Ffotos%2F&psig=AOvVaw35Msq5B31JpXeDkCRiAc2O&ust=1634260577949000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCKj-65zdyPMCFQAAAAAdAAAAABAE"
       />
     </li>
     <li>
       <h1>Dog Rock</h1>
       <img
-        class="poster"
-        src="img\proximos_estrenos\rock_dog_poster_480x670.png"
+        class="poster" height="450"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gatotv.com%2Fpelicula%2Frock_dog_el_perro_rockero%2Fposter&psig=AOvVaw32vu0OsfqnK-zRQhn-8lct&ust=1634260604104000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCKijj6ndyPMCFQAAAAAdAAAAABAK"
       />
     </li>
     <li>
       <h1>Ron da Error</h1>
       <img
-        class="poster"
-        src="img\proximos_estrenos\ron_da_error_poster_480x670.png"
+        class="poster" height="450"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Frevistayume.com%2Fpost%2Fron-da-error-primer-trailer-y-poster&psig=AOvVaw31mbnY1SqNfFvZ4YqbwTPh&ust=1634260678364000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCODIxc3dyPMCFQAAAAAdAAAAABAN"
       />
     </li>
   </ul>
