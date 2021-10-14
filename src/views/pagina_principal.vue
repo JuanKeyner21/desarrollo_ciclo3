245392493e0b13f9b3552daf3304c63947f97780
Juan Pablo <juan.diaz3@o365.unab.edu.co>
2021-10-13 22:35:15 -0500

    Se corrigieron las imágenes de la página principal

--- src/views/pagina_principal.vue
+++ src/views/pagina_principal.vue
@@ -7,15 +7,15 @@
   <ul>
     <li>
       <h1>La Última Estafa</h1>
-      <img class="poster" height="450" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cinecolombia.com%2Fpeliculas%2Fla-ultima-estafa&psig=AOvVaw2zpN0EdPwd9j7Lj9efbSGa&ust=1634260418845000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCKin7NLcyPMCFQAAAAAdAAAAABAD" />
+      <img class="poster" height="450" src="https://static.cinepolis.com/resources/mx/movies/posters/414x603/36492-123943-20210707050600.jpg" />
     </li>
     <li>
       <h1>Shang-Chi</h1>
-      <img class="poster" height="450" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fnoescinetodoloquereluce.com%2F2021%2F08%2Fnuevo-poster-de-shang-chi-y-la-leyenda-de-los-diez-anillos-la-cinta-de-artes-marciales-de-marvel.html&psig=AOvVaw3Oo3qMpSAaZr3ncs9NABTt&ust=1634260523721000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCIjFmIPdyPMCFQAAAAAdAAAAABAD" />
+      <img class="poster" height="450" src="https://i0.wp.com/noescinetodoloquereluce.com/wp-content/uploads/2021/08/shangchi_and_the_legend_of_the_ten_rings.jpg?resize=800%2C1185&ssl=1" />
     </li>
     <li>
       <h1>Maligno</h1>
-      <img class="poster" height="450" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dodmagazine.es%2Fmaligno-poster-oficialpelicula-terror-james-wan%2F&psig=AOvVaw1psJYyg0kQTm96epz--a9X&ust=1634260549310000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCNCgn5DdyPMCFQAAAAAdAAAAABAD" />
+      <img class="poster" height="450" src="https://www.dodmagazine.es/wp-content/uploads/2021/07/maligno-pelicula-terror-james-wan.jpg" />
     </li>
   </ul>
 
@@ -25,21 +25,21 @@
       <h1>Sin Tiempo para morir</h1>
       <img
         class="poster" height="450"
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sensacine.com%2Fpeliculas%2Fpelicula-212358%2Ffotos%2F&psig=AOvVaw35Msq5B31JpXeDkCRiAc2O&ust=1634260577949000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCKj-65zdyPMCFQAAAAAdAAAAABAE"
+        src="https://es.web.img3.acsta.net/pictures/21/09/09/10/40/0885856.jpg"
       />
     </li>
     <li>
       <h1>Dog Rock</h1>
       <img
         class="poster" height="450"
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gatotv.com%2Fpelicula%2Frock_dog_el_perro_rockero%2Fposter&psig=AOvVaw32vu0OsfqnK-zRQhn-8lct&ust=1634260604104000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCKijj6ndyPMCFQAAAAAdAAAAABAK"
+        src="https://imagenes.gatotv.com/categorias/peliculas/posters/rock_dog.jpg"
       />
     </li>
     <li>
       <h1>Ron da Error</h1>
       <img
         class="poster" height="450"
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Frevistayume.com%2Fpost%2Fron-da-error-primer-trailer-y-poster&psig=AOvVaw31mbnY1SqNfFvZ4YqbwTPh&ust=1634260678364000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCODIxc3dyPMCFQAAAAAdAAAAABAN"
+        src="https://revistayume.com/uploads/media/2021/06/Ron-da-Error-teaser-poster.jpg"
       />
     </li>
   </ul>
